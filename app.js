e0f08902aac2a8551ad211aa822a5e82e812c803
Jorge Paul <jserranopaul@gmail.com>
2022-02-22 12:09:27 +0100

    ES modules

---
+++ app.js
@@ -0,0 +1,6 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules
+// import: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import
+import { sayHello, sayGoodBye } from './utils/utils.js';
+
+sayHello();
+sayGoodBye();
---
+++ utils/utils.js
@@ -0,0 +1,3 @@
+// export: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/export
+export const sayHello = () => console.log('hello');
+export const sayGoodBye = () => console.log('goodbye');
